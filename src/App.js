abf6d7caa3b5616d007c24526bcf65fd3a2ac906
Breno Vergílio <brenoverglio@pop-os.localdomain>
2021-09-01 00:05:39 -0400

    responsivity

--- src/App.js
+++ src/App.js
@@ -57,6 +57,12 @@ export default () => {
         Direitos de imagem para Netflix<br/>
         Dados pegos do site themoviedb.org
       </footer>
+
+      {movieList.length <= 0 &&
+      <div className="loading">
+        <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="carregando"></img>
+      </div>
+      }
     </div>
   );
 }
